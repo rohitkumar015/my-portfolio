e79066b425044629d216dfad2c43677f730c76f3
rohitkumar67 <rohitp@hyplap.com>
2024-05-08 00:41:24 +0530

    project card updated

--- src/component/Projectcards.jsx
+++ src/component/Projectcards.jsx
@@ -78,7 +78,7 @@ const Projectcards = () => {
         </div>
         <div className="col-lg-6 col-md-6 col-12  position-relative">
           <img
-            src="https://images.unsplash.com/photo-1616530940355-351fabd9524b?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          src="/images/business.jpg"
             alt=""
             className="project_image mt-2"
           />
