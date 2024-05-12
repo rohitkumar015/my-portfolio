3e4bddf9d4c3c7e9165db8d9bec7c57e3ff344f6
rohitkumar67 <rohitp@hyplap.com>
2024-05-12 13:36:58 +0530

    gradient added ,...no changed in performance

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ function App() {
   return (
     <>
     <div className='body_css position-relative  primary_bg'>
-    
+    <div className="gradient_one"></div>
+        <div className="gradient_two"></div>
     <main className='container'>
     <Homepage></Homepage>
     </main>
